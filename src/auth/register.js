5de68c3228a3e6559c640bae1b70b24f0eda6a04
masan <masanu2010@hotmail.com>
2024-05-03 01:28:50 -0600

    Login Service

--- src/auth/register.js
+++ src/auth/register.js
@@ -1,10 +1,29 @@
 const express = require("express");
 const {json} = require("express");
-const router= express.Router();
+const router = express.Router();
+const db = require("../../models")
+const {Usuario} = require("../../models")
+router.post("/register", (req, res, next) => {
+    let nombre = req.body.nombre;
+    let password = req.body.password;
+    let email = req.body.email;
 
-router.get("/register", (req, res)=>{
-    res.json({message: "hola"})
-    res.status(200)
+    try {
+        Usuario.create({
+            nombre: nombre,
+            password: password,
+            email: email
+        }).then((data) => {
+            res.status(201).send("Created");
+        }).catch((error) => {
+            next(error);
+        })
+    } catch (error) {
+        console.log(error.message);
+        res.status(500);
+        res.send(error);
+    }
+
 });
 
 
