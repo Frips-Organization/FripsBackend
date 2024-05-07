d381162aa0e2108ca2d627097cff5ff6fdf51b74
Diego <jdapuy@gmail.com>
2024-05-06 22:55:25 -0600

    Feature: Configuracion de cors

--- src/app.js
+++ src/app.js
@@ -1,11 +1,14 @@
-const express = require("express")
-const authenticationMiddleware = require("./auth/authentication.middleware")
+const express = require("express");
+const cors = require("cors");
+const authenticationMiddleware = require("./auth/authentication.middleware");
 const app = express();
 
-const auth = require("./auth")
-const api = require("./api")
-const {json, urlencoded} = require("body-parser");
+const auth = require("./auth");
+const api = require("./api");
+const { json, urlencoded } = require("body-parser");
 
+// Configuración de CORS
+app.use(cors());
 
 app.use(json());
 app.use(urlencoded());
