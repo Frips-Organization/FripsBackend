812def4be39e4c61569244ec9ee1fabf98067671
jdapuy <32533395+jdapuy@users.noreply.github.com>
2024-06-01 00:49:43 -0600

    Fix: findone de plans

--- src/api/plan/plan.js
+++ src/api/plan/plan.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const db = require("../../../models");
 const { Plan } = require("../../../models");
-const {Lugar} = require("../../../models");
+const { Lugar } = require("../../../models");
 const { Itinerario } = require("../../../models");
 const router = express.Router();
 
@@ -21,26 +21,25 @@ router.post("/plan", async (req, res, next) => {
   //let updatedAt
 
   try {
-
-     const plan = await Plan.create({
-      itinerarioId : itinerarioId,
-      userId : userId,
+    const plan = await Plan.create({
+      itinerarioId: itinerarioId,
+      userId: userId,
       nombrelugar: nombrelugar,
-      descripcion : descripcion,
-      horaLlegada : horaLlegada,
-      horaSalida : horaSalida,
+      descripcion: descripcion,
+      horaLlegada: horaLlegada,
+      horaSalida: horaSalida,
       puntoPartida: puntoPartida,
-      motivo: motivo
-     });
+      motivo: motivo,
+    });
 
     //Aqui hace falta verificar que si el nombre del lugar ya
     //existe en la tabla "Lugar", entonces se salte la siguiente insercion
 
     const lugar = await Lugar.create({
-      nombre : nombrelugar,
-      descripcion : "sin descripcion", //Estos son valores por defecto para un nuevo lugar
-      ubicacion : "sin especificar"
-    })
+      nombre: nombrelugar,
+      descripcion: "sin descripcion", //Estos son valores por defecto para un nuevo lugar
+      ubicacion: "sin especificar",
+    });
 
     res.status(201).send("Created");
   } catch (error) {
@@ -49,32 +48,28 @@ router.post("/plan", async (req, res, next) => {
   }
 });
 
+router.get("/plan/:itinerarioId", async (req, res, next) => {
+  const { itinerarioId } = req.params;
 
-router.get("/plan/:itinerarioId", async(req,res,next) => {
-const {itinerarioId} = req.params;
+  try {
+    const planes = await Itinerario.findOne({
+      where: { itinerarioId },
+      include: [
+        {
+          model: Plan,
+        },
+      ],
+    });
 
-try {
+    if (!planes) {
+      return res.status(404).send("Plane no econtrado");
+    }
 
-  const planes = await Itinerario.finOne({
-    where : { itinerarioId },
-    include: [
-      {
-        model: Plan,
-      },
-    ],
-  });
-
-  if(!planes) {
-    return res.status(404).send("Plane no econtrado");
+    res.json({ planes });
+  } catch (error) {
+    console.error(error);
+    res.status(500).send("Error interno del servidor");
   }
-
-  res.json({ planes });
-  
-}catch (error) {
-  console.error(error);
-  res.status(500).send("Error interno del servidor");
-}
-
 });
 
 module.exports = router;
